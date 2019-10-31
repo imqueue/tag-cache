558534837d2f7cc11b56b18a1a739cb7c772372b
Mykhailo Stadnyk <mikhus@gmail.com>
2019-10-31 17:09:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# @imqueue/tag-cache
+
+[![Build Status](https://travis-ci.org/imqueue/tag-cache.svg?branch=master)](https://travis-ci.org/imqueue/tag-cache)
+[![David](https://img.shields.io/david/imqueue/tag-cache.svg)](https://david-dm.org/imqueue/tag-cache)
+[![David](https://img.shields.io/david/dev/imqueue/tag-cache.svg)](https://david-dm.org/imqueue/tag-cache?type=dev)
+[![Known Vulnerabilities](https://snyk.io/test/github/imqueue/tag-cache/badge.svg?targetFile=package.json)](https://snyk.io/test/github/imqueue/tag-cache?targetFile=package.json)
+[![License](https://img.shields.io/badge/license-ISC-blue.svg)](https://rawgit.com/imqueue/sequelize/master/LICENSE)
+
+Tagged Cache implementation over redis for @imqueue
+
+# Install
+
+~~~bash
+npm i --save @imqueue/tag-cache
+~~~
+
+# Docs
+
+~~~bash
+git clone git@github.com:imqueue/tag-cache.git
+npm run docs
+~~~
+
---
+++ index.ts
@@ -0,0 +1,162 @@
+/*!
+ * @imqueue/tag-cache - Tagged Cache implementation over redis for @imqueue
+ *
+ * Copyright (c) 2019, imqueue.com <support@imqueue.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+import { ILogger, IRedisClient, RedisCache } from '@imqueue/rpc';
+
+/**
+ * Empty function used to ignore promises, for cases, when we do not care
+ * about results and just want to execute some routines in background
+ */
+function ignore() { /* do nothing */ }
+
+// noinspection JSUnusedGlobalSymbols
+export class TagCache {
+
+    private logger: ILogger;
+    private redis: IRedisClient;
+    private readonly key: (key: string) => string;
+
+    // noinspection TypeScriptUnresolvedVariable,JSUnusedGlobalSymbols
+    /**
+     * @constructor
+     * @param {RedisCache} cache
+     */
+    constructor(private cache: RedisCache) {
+        this.logger = (this.cache as any).logger;
+        this.redis = (RedisCache as any).redis;
+        this.key = (this.cache as any).key.bind(this.cache);
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * Returns data stored under given keys. If a single key provided will
+     * return single result, otherwise will return an array of results
+     * associated with the keys
+     *
+     * @param {string[]} keys
+     * @return Promise<any | null | Array<any | null>>
+     */
+    public async get(
+        ...keys: string[]
+    ): Promise<any | null | Array<any | null>> {
+        try {
+            if (keys.length === 1) {
+                const value: string = await this.redis.get(
+                    this.key(keys[0]),
+                ) as any as string;
+
+                return value ? JSON.parse(value) : null;
+            }
+
+            return (await this.redis.mget(
+                keys.map(key => this.key(key)),
+            ) as any as string[]).map((value: string) =>
+                value ? JSON.parse(value) : null
+            );
+        } catch (err) {
+            this.logger.warn('Cache get error:', err.stack);
+
+            return null;
+        }
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * Stores given value under given kay, tagging it with the given tags
+     *
+     * @param {string} key - name of the key to store data under
+     * @param {any} value - data to store in cache
+     * @param {string[]} tags - tag strings to mark data with
+     * @param {number} [ttl] - TTL in milliseconds
+     */
+    public async set(
+        key: string,
+        value: any,
+        tags: string[],
+        ttl?: number,
+    ): Promise<boolean> {
+        try {
+            const multi = this.redis.multi();
+            const setKey = this.key(key);
+
+            for (const tag of tags) {
+                multi.sadd(this.key(`tag:${tag}`), setKey);
+            }
+
+            if (ttl) {
+                multi.set(setKey, JSON.stringify(value), 'PX', ttl);
+            } else {
+                multi.set(setKey, JSON.stringify(value));
+            }
+
+            await multi.exec();
+
+            return true;
+        } catch (err) {
+            this.logger.warn('Cache set error:', err.stack);
+
+            return false;
+        }
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * Invalidates data under given tags
+     *
+     * @param {string[]} tags
+     * @return {Promise<boolean>}
+     */
+    public async invalidate(...tags: string[]): Promise<boolean> {
+        try {
+            const keys: string[] = ([] as string[]).concat(
+                await Promise.all(tags.map(tag =>
+                    this.redis.smembers(this.key(`tag:${tag})`)) as any as
+                        Promise<string>
+                )) as any as string[],
+            );
+
+            const pipe = await this.redis.pipeline();
+
+            for (const key of keys) {
+                pipe.del(this.key(key)).then(ignore).catch(ignore);
+            }
+
+            for (const tag of tags) {
+                pipe.del(this.key(`tag:${tag}`)).then(ignore).catch(ignore);
+            }
+
+            await pipe.exec();
+
+            return true;
+        } catch (err) {
+            this.logger.warn('Cache invalidate error:', err.stack);
+
+            return false;
+        }
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * Destroys this cache instance
+     */
+    public async destroy(): Promise<void> {
+        await RedisCache.destroy();
+
+        delete this.redis;
+        delete this.cache;
+    }
+}
